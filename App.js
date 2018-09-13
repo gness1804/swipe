62e5717a1816b957c4d66256d49645a112defa65
gness1804 <graham.nessler@gmail.com>
2018-09-12 20:42:51 -0500

    Update dummy data and renderCard function

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import Deck from './src/Deck';
 import styles from './src/styles/app';
+import data from './src/data/dummy';
 
 export default class App extends React.Component {
+  renderCard(item) {
+    return (
+        <Text>{item.text}</Text>
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Deck />
+        <Deck
+            data={data}
+            renderCard={this.renderCard}
+        />
       </View>
     );
   }
